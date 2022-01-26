cc2109dfac6b5320d51dcf85bc20e0b37b3d9449
Christian Figueiredo <developer.figueiredo@gmail.com>
2022-01-26 09:29:11 +0000

    autenticacao do usuario

--- TelaCadastro/script.js
+++ TelaCadastro/script.js
@@ -12,4 +12,39 @@ let btn = document.querySelector('.fa-eye')
                 }
     })
     
-    
+   function entrar(){
+        let usuario = document.querySelector('#usuario')
+        let userLabel = document.querySelector('#userLabel')
+
+        let senha = document.querySelector('#senha')
+        let senhaLabel = document.querySelector('#senhaLabel')
+
+        let msgError = document.querySelector('#msgError')
+        let listaUser = []
+
+        let userValid = {
+            nome: '',
+            usuario: '',
+            senha: ''
+        }
+        listaUser = JSON.parse(localStorage.getItem('listaUser'))
+        listaUser.forEach((item)=> {
+            if(usuario.value == item.usuario && senha.value == item.senha){
+                userValid = {
+                    nome: item.nome,
+                    usuario: item.usuario,
+                    senha: item.senha
+                }
+            }
+        })
+
+        if(usuario.value == userValid.usuario && senha.value==userValid.senha){
+            alert('deu certo')
+        }
+        else{
+            alert('Deu ruim')
+        }
+
+        
+      
+   } 
